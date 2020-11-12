acb724846a0da77689094dce96ec85296243e0f4
jamiemalcolm <69213587+jamiemalcolm@users.noreply.github.com>
2020-11-12 11:24:39 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+ const items = document.querySelectorAll('.item');
+
+// function tglOpen() {
+//   console.log('hello');
+// }
+
+// items.forEach(item =>item.addEventListener('click', tglOpen));
+
+
+
+
+
+    function toggleOpen() {
+      console.log('clicked');
+      this.classList.toggle('open');
+    }
+    function toggleActive(e) {
+      if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+        console.log(e); 
+    }
+  }
+
+
+
+    items.forEach(item =>item.addEventListener('click', toggleOpen));
+    items.forEach(item =>item.addEventListener('transitionend', toggleActive));
